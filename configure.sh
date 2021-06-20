de5400f4187d31e219ac0a224b666c5abc16c5be
lhllhx <52107206+lhllhx@users.noreply.github.com>
2021-06-20 17:46:29 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -9,6 +9,24 @@ web_server:
   run_v2: yes #启动 V2 版本  
 schedules: no
 
+tasks:  
+  ob: #任务名称，改冒号前的。
+    rss: 
+      url: https://google.com
+      other_fields:
+        - link
+    accept_all: yes 
+    #content_size: #启用大小过滤
+      #max: 9999 # 文件大于 9999M 就不下载
+      #strict: no #不要动
+    qbittorrent:
+      host: 127.0.0.1
+      port: 8080
+      use_ssl: false
+      verify_cert: false
+      username: admin
+      password: $1
+      maxupspeed: 25000
 EOF
 flexget web passwd "Aaa123456"
 flexget daemon start
