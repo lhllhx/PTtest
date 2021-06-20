c7a3aa57ba9a593c79a2299b0f3d9160ab828236
lhllhx <52107206+lhllhx@users.noreply.github.com>
2021-06-20 17:42:28 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,7 +4,7 @@ touch /.flexget/config.yml
 cat << EOF >>/.flexget/config.yml
 web_server:
   bind: 0.0.0.0
-  port: 443 #web-ui的监听端口
+  port: 8080 #web-ui的监听端口
   web_ui: yes #启用web-ui
   run_v2: yes #启动 V2 版本  
 schedules: no
