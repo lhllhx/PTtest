67a213998de72a4434edecefd6b85b227833899f
lhllhx <52107206+lhllhx@users.noreply.github.com>
2021-08-05 22:59:39 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,44 +1,16 @@
 mkdir .flexget
 mkdir data
-touch /.flexget/config.yml
 mkdir .flexget/plugins
-cd ./.flexget/plugins
-git clone https://github.com/lhllhx/flexget_qbittorrent_mod.git
-cd ./data
-touch db-config.sqlite        
-7z x -aoa -p$zip config.zip
-rm -f  config.zip
-cp -r  config.yml ~/.flexget
-cp -r  db-config.sqlite ~/.flexget
+git clone https://github.com/lhllhx/flexget_qbittorrent_mod.git ~/.flexget/plugins
+cd ./.flexget
+touch /.flexget/config.yml
+touch /.flexget/db-config.sqlite 
+git clone https://${GITHUB_TOKEN}@github.com/${DATE}.git ~/data
+cd ~/data
+cp -r  ./config.yml ~/.flexget
+cp -r  ./db-config.sqlite ~/.flexget
 rm -f  db-config.sqlite
 rm -f  config.yml
 rm -f  *.log
-cat << EOF >>/.flexget/config.yml
-web_server:
-  bind: 0.0.0.0
-  port: ${PORT} #web-ui的监听端口
-  web_ui: yes #启用web-ui
-  run_v2: yes #启动 V2 版本  
-schedules: no
-
-tasks:  
-  ob: #任务名称，改冒号前的。
-    rss: 
-      url: https://google.com
-      other_fields:
-        - link
-    accept_all: yes 
-    #content_size: #启用大小过滤
-      #max: 9999 # 文件大于 9999M 就不下载
-      #strict: no #不要动
-    qbittorrent:
-      host: 127.0.0.1
-      port: 8080
-      use_ssl: false
-      verify_cert: false
-      username: admin
-      password: aaaaaaaa
-      maxupspeed: 25000
-EOF
-flexget web passwd "Aaa258369!flexget"
+flexget web passwd "${PASSWORD}"
 flexget daemon start -d
